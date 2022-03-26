a874b026a879dd58d5863c28cb67b2a53c4f24e9
YOUR NAME <jhanov@gmail.com>
2022-03-25 18:03:42 -0700

    add inputs to meme components

---
+++ src/components/Meme.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Meme() {
+
+
+  return (
+    <section className="meme">
+      <div className="meme-inputs">
+        <input placeholder="Shut up" type="text" />
+        <input placeholder="And take my money" type="text" />
+      </div>
+
+      <button> Get a new meme image </button>
+    </section>
+  )
+}
+
+
