3656d048e69a6f79b518ecd8898ea0a7e2b6a3e2
YOUR NAME <jhanov@gmail.com>
2022-03-25 20:12:08 -0700

    add memesData with popular meme data implement on click for button

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import memesData from '../memesData.js'
 
 export default function Meme() {
 
+  function clickAction() {
+    console.log("hey")
+  }
 
   return (
     <form className="meme">
       <input className="meme-input" placeholder="Shut up" type="text" />
       <input className="meme-input" placeholder="And take my money" type="text" />
-      <button className="meme-button"> Get a new meme image </button>
+      <button onClick={clickAction} className="meme-button"> Get a new meme image </button>
     </form>
-
   )
 }
 
