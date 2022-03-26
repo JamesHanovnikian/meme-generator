00c8e587d9465f11e80d2cdb200a1dd8955260a7
YOUR NAME <jhanov@gmail.com>
2022-03-26 14:52:12 -0700

    add functionality to change state of meme image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,17 +2,26 @@ import React from 'react'
 import memesData from '../memesData.js'
 
 export default function Meme() {
+  let url
+
+  const [memeImage, setMemeImage] = React.useState("")
 
   function clickAction() {
     console.log("hey")
+    let i = 0
+    let memes = memesData["data"]["memes"]
+    let randomMeme = memes[Math.floor(Math.random() * memes.length)]
+    url = randomMeme["url"]
+    setMemeImage(url)
   }
 
   return (
-    <form className="meme">
+    <div className="meme">
       <input className="meme-input" placeholder="Shut up" type="text" />
       <input className="meme-input" placeholder="And take my money" type="text" />
       <button onClick={clickAction} className="meme-button"> Get a new meme image </button>
-    </form>
+      <img className="meme-image" src={memeImage} />
+    </div>
   )
 }
 
