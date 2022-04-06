fcde242f1af5f0492cb7e36d0b87aa0a0d7a6223
YOUR NAME <jhanov@gmail.com>
2022-04-06 13:20:03 -0500

    refactor state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -4,15 +4,26 @@ import memesData from '../memesData.js'
 export default function Meme() {
   let url
 
-  const [memeImage, setMemeImage] = React.useState("")
+  // const [memeImage, setMemeImage] = React.useState()
+
+  const [memeImage, setMemeImage] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "https://i.imgflip.com/30b1gx.jpg"
+  })
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
   function clickAction() {
     console.log("hey")
     let i = 0
-    let memes = memesData["data"]["memes"]
+    let memes = allMemeImages["data"]["memes"]
     let randomMeme = memes[Math.floor(Math.random() * memes.length)]
     url = randomMeme["url"]
-    setMemeImage(url)
+    setMemeImage(prevMeme => ({
+      ...prevMeme,
+      randomImage: url
+    }))
   }
 
   return (
@@ -20,7 +31,7 @@ export default function Meme() {
       <input className="meme-input" placeholder="Shut up" type="text" />
       <input className="meme-input" placeholder="And take my money" type="text" />
       <button onClick={clickAction} className="meme-button"> Get a new meme image </button>
-      <img className="meme-image" src={memeImage} />
+      <img className="meme-image" src={memeImage.randomImage} />
     </div>
   )
 }
